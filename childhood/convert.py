1ab75d0fa47577f5567e404732dc939115c5d06f
Sam Phippen <samphippen@googlemail.com>
2012-04-30 16:18:57 +0100

    Removed convert.py because It's not music
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- childhood/convert.py
+++
@@ -1,8 +0,0 @@
-import os
-
-if __name__ == "__main__":
-    for file in os.listdir("."):
-        i = file
-        o = file.replace(".wav",".mp3")
-        os.system("lame -V0 \"" + i + "\" \"" + o + "\"")
-        
